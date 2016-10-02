fd52ecc3ea354bb24e81876491056dfdaf877093
arunmvit <arunmvit@gmail.com>
2016-10-02 20:41:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artile-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('article one requested..');
 });
 
-app.get('/artile-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('article two requested..');
 });
 
-app.get('/artile-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('article three requested..');
 });
 
