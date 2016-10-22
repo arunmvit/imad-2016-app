8ecf4b3c7b494d3e50059277132dec1b6f58d1c8
arunmvit <arunmvit@gmail.com>
2016-10-22 08:54:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var counter = 0;
+var but = document.getElementById('counter');
+but.onClick = function(){
+    counter=+1;
+    var span = document.getElementById('count');
+    span.innerHtml = counter;
+};
